20eb2c215f9d4c11bf4cba3d8377bdd92c726ce3
somwanshi.dnyaneshwar@gmail.com <Mahatma@352>
2021-05-07 11:31:44 +0530

    Initial commit

---
+++ src/app/pages/extra-components/sample/sample.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-sample',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.scss']
+})
+export class SampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
